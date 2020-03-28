97f4516ff793a478efd0485a02e92b9d16233ec3
Dedicio Coelho <dedicio@gmail.com>
2020-03-28 19:35:13 +0000

    completado desafio 2

--- desafio2.js
+++ desafio2.js
@@ -3,14 +3,19 @@
     - A função deve receber 2 argumentos;
     - O retorno das funcão deve ser a soma dos dois parametros passados.
 */
-exports.sum = null
+
+exports.sum = (num1, num2) => {
+    return num1 + num2;
+};
 
 /*
  2. Crie uma função com as seguintes características:
     - A função deve receber 2 argumentos, nome e sobrenome;
     - O retorno das funcão deve ser juncão(cancatenacão) dos dois parametros passados e retonar o nome completo.
 */
-exports.fullname = null;
+exports.fullname = (nome, sobrenome) => {
+    return `${nome} ${sobrenome}`;
+};
 
 /*
  3. Crie uma função com as seguintes características:
@@ -19,7 +24,14 @@ exports.fullname = null;
       - "Preencha todos os valores corretamente!"
     - O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
-exports.calculate = null
+exports.calculate = (num1, num2, num3) => {
+
+    if (!num1 || !num2 || !num3) {
+        return 'Preencha todos os valores corretamente!';
+    }
+    
+    return (num1 * num2 * num3) + 2;
+}
 
 /*
  4. Crie uma função com as seguintes características:
@@ -29,11 +41,34 @@ exports.calculate = null
     - Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
     - Se nenhum argumento for passado, retorne o valor booleano `false`.
 */
-exports.calculatev2 = null
+exports.calculatev2 = (num1, num2, num3) => {
+
+	const fullParams = [num1, num2, num3].filter(item => item);	
+	let value = false;
+	
+	switch (fullParams.length) {
+		case 3:
+			value = (num1 + num2) / num3;
+			break;
+	
+		case 2:
+			value = fullParams[0] + fullParams[1];
+			break;
+
+		case 1:
+			value = fullParams[0];
+			break;
+	}
+
+	return value;
+}
 
 /*
  5. Crie uma função com as seguintes características:
     - A função deve receber 1 argumento;
     - O retorno das funcão deve ser um boolean indicando se o numero passado é um numero par ou não.
 */
-exports.isPair = null
+exports.isPair = (number) => {
+
+	return !(number % 2);
+}
