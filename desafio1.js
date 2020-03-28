961446310ad847e9b9391626c8f06399bdf85006
Dedicio Coelho <dedicio@gmail.com>
2020-03-28 18:53:51 +0000

    completado desafio1

--- desafio1.js
+++ desafio1.js
@@ -1,9 +1,20 @@
 // 1. Declare uma nova variável chamada `name` com o nome `Anderson` e exporte ela como `name`.
+const name = 'Anderson';
 
 // 2. Declare uma nova variável chamada `soma`, adicione uma instrução somando os valores 15 e 8, e exporte ela como `sum`.
+const soma = 15 + 8;
 
 // 3. Declare uma nova variável chamada `numbers` e atribua um array com os numeros de '1',  '5' e '10', e exporte ela como `numbers`.
 
-// 4. Declare uma nova variável chamada `isBeautiful` atribua à ela o valor booleano que representa `verdadeiro`, e exporte ela como `isBeautiful`.
+const numbers = [1,5,10];
 
 // 4. Declare uma nova variável chamada `isBeautiful` atribua à ela o valor booleano que representa `verdadeiro`, e exporte ela como `isBeautiful`.
+const isBeautiful = true;
+
+
+module.exports = {
+    name,
+    sum: soma,
+    numbers,
+    isBeautiful
+}
