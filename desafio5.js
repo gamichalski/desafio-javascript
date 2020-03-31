fa3add43e21c2770af4e5656d753bc6ab5a27138
edianassouza <edianadasilvadesouza@gmail.com>
2020-03-31 17:46:51 +0000

    desafio 5 completo

--- desafio5.js
+++ desafio5.js
@@ -7,5 +7,33 @@
     - A pesquisa deve desconsiderar se o ingrediente contem letras maiusculas, ou seja;
       - Se pesquisar: Queijo, queijo ou QuEiJo. O resultado deve ser "Encontrado"
 */
+function ingredientsFilter(arg1){
+    const lista = require('./ingredients.js')
+    arg1 = arg1.toLowerCase()
 
-exports.ingredientsFilter = null
+    //Pelo que eu entendi, se usar find ou filter o retorno vai ser um array. Logo, não posso usar dessa forma
+    
+    /*
+    const itemFilterObj = lista.find((ingrediente) => {
+        if (ingrediente.includes(arg1)){
+            return "Encontrado"
+        }
+    });
+    
+    return itemFilterObj
+    
+    */
+
+    //Assim, é necessário fazer pela forma mais "verbosa"
+
+    for (let count in lista){
+        lista[count] = lista[count].toLowerCase();
+        if (lista[count] == arg1){
+            return "Encontrado"
+        }
+    }
+    return "Não Encontrado"
+    
+    
+}
+exports.ingredientsFilter = ingredientsFilter
